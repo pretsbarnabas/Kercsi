835409c18dd1f011313a127a42d0845410ff3b98
Pozsgai Bence <pozsgai.bence@students.jedlik.eu>
2023-04-21 09:52:26 +0200

    ÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁ

--- Kercsi/MainWindow.xaml.cs
+++ Kercsi/MainWindow.xaml.cs
@@ -26,6 +26,59 @@ namespace Kercsi
         {
             InitializeComponent();
             mainTable = new();
+            FillGrid();
+            
+
+        }
+        public void FillGrid()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    if (mainTable.tileValues[y, x].ToString() == "Forest")
+                    {
+                        Image image = new Image();
+                        image.Source = new BitmapImage(new Uri("/wood.png", UriKind.Relative));
+                        Grid.SetRow(image, x);
+                        Grid.SetColumn(image, y);
+                        Gr_Table.Children.Add(image);
+                    }
+                    else if (mainTable.tileValues[y, x].ToString() == "Meadow")
+                    {
+                        Image image = new Image();
+                        image.Source = new BitmapImage(new Uri("/ret.png", UriKind.Relative));
+                        Grid.SetRow(image, x);
+                        Grid.SetColumn(image, y);
+                        Gr_Table.Children.Add(image);
+                    }
+                    else if (mainTable.tileValues[y, x].ToString() == "Mountain")
+                    {
+                        Image image = new Image();
+                        image.Source = new BitmapImage(new Uri("/hegy xd.png", UriKind.Relative));
+                        Grid.SetRow(image, x);
+                        Grid.SetColumn(image, y);
+                        Gr_Table.Children.Add(image);
+                    }
+                    else if (mainTable.tileValues[y, x].ToString() == "Hill")
+                    {
+                        Image image = new Image();
+                        image.Source = new BitmapImage(new Uri("/claydomb.png", UriKind.Relative));
+                        Grid.SetRow(image, x);
+                        Grid.SetColumn(image, y);
+                        Gr_Table.Children.Add(image);
+                    }
+                    else if (mainTable.tileValues[y, x].ToString() == "None")
+                    {
+
+                        Image image = new Image();
+                        image.Source = new BitmapImage(new Uri("/sand.jpg", UriKind.Relative));
+                        Grid.SetRow(image, x);
+                        Grid.SetColumn(image, y);
+                        Gr_Table.Children.Add(image);
+                    }
+                }
+            }
         }
     }
 }
